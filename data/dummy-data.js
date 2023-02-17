9322a38e7c0815b8c0cb7fd2e66467211cacbb42
twój login do GitHuba <twj email>
2023-02-17 18:16:00 +0100

    making a grid with styling and navigation

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,5 +1,5 @@
-import Category from '../models/category';
-import Meal from '../models/meal';
+import Category from '../assets/models/category';
+import Meal from '../assets/models/meal';
 
 export const CATEGORIES = [
   new Category('c1', 'Italian', '#f5428d'),
