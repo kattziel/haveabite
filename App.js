8006c6df657f60d5d1fe236ed42a812043a93606
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-02-21 16:54:20 +0100

    providing navigation

--- App.js
+++ App.js
@@ -4,18 +4,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          </Stack.Navigator>
-        {/* <CategoriesScreen /> */}
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,17 @@
 import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={styles.gridItem}>
-      <Pressable android_ripple={'#ccc'} style={({pressed}) => [styles.button, pressed ? styles.buttonPressed : null ]}>
-        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+      <Pressable
+        android_ripple={"#ccc"}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
@@ -15,35 +22,35 @@ function CategoryGridTile({ title, color }) {
 export default CategoryGridTile;
 
 const styles = StyleSheet.create({
-    gridItem: {
-        flex: 1,
-        margin: 16,
-        height: 150,
-        borderRadius: 8,
-        elevation: 4,
-        backgroundColor: 'white',
-        shadowColor: 'black',
-        shadowOpacity: 0.25,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
-        // for ios shadow is outside attribute, hence hidden makes it invisible
-    },
-    button: {
-        flex: 1
-    },
-    innerContainer: {
-        flex: 1,
-        padding: 16,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 8
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18
-    },
-    buttonPressed: {
-        opacity: 0.5
-    }
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    // for ios shadow is outside attribute, hence hidden makes it invisible
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
 });
