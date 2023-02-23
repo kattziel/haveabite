08465a4ecda2657b6b1886320f3c0627d7a1a3d0
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-02-23 18:53:37 +0100

    empty favorites screen styling

--- components/FavoritesScreen.js
+++ components/FavoritesScreen.js
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import MealsList from './MealsList/MealsList';
 import {FavoritesContext} from '../store/context/favorites-context';
 import { MEALS } from '../data/dummy-data';
+import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native-web';
 
 function FavoritesScreen() {
 
@@ -10,8 +12,27 @@ function FavoritesScreen() {
     const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
     // we check whether id of the meal is included in favoriteMealsCtx.ids; if so - it is shown to the screen;
 
+    if (favoriteMeals.length === 0) {
+        return <View style={styles.rootContainer}>
+            <Text style={styles.text}> You have no favorites yet ! </Text>
+        </View>
+    }
+
     return <MealsList items={favoriteMeals}/>
 
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white'
+    }
+})
