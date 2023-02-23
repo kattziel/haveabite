bd245845f8232a73d4d0780a508bf63d7b1deeae
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-02-23 18:47:46 +0100

    creating a favorites screen

--- components/FavoritesScreen.js
+++ components/FavoritesScreen.js
@@ -1,7 +1,16 @@
-import {Text} from 'react-native';
+import { useContext } from 'react';
+import MealsList from './MealsList/MealsList';
+import {FavoritesContext} from '../store/context/favorites-context';
+import { MEALS } from '../data/dummy-data';
 
 function FavoritesScreen() {
-    return <Text> Favorites screen! </Text>
+
+    const favoriteMealsCtx = useContext(FavoritesContext);
+
+    const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+    // we check whether id of the meal is included in favoriteMealsCtx.ids; if so - it is shown to the screen;
+
+    return <MealsList items={favoriteMeals}/>
 
 }
 
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,6 @@
 import { MEALS, CATEGORIES } from "../data/dummy-data";
-import { View, FlatList, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
-
-import MealItem from "../components/MealItem";
+import MealsList from "../components/MealsList/MealsList";
 
 function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
@@ -21,37 +19,10 @@ function MealsOverviewScreen({ route, navigation }) {
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
+  return <MealsList items={displayedMeals}/>
 
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration,
-    };
-
-    return <MealItem {...mealItemProps} />;
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      ></FlatList>
-    </View>
-  );
-}
+};
 
 export default MealsOverviewScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
+
